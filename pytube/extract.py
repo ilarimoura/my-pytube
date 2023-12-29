d0f057d1d683c2bb999bf24198417ef93c9573f8
larissamoura <larissarm91@gmail.com>
2023-12-29 00:08:33 -0300

    correcoes pytube

--- pytube/extract.py
+++ pytube/extract.py
@@ -541,7 +541,6 @@ def initial_player_response(watch_html: str) -> str:
         pattern='initial_player_response_pattern'
     )
 
-
 def metadata(initial_data) -> Optional[YouTubeMetadata]:
     """Get the informational metadata for the video.
 
@@ -558,22 +557,12 @@ def metadata(initial_data) -> Optional[YouTubeMetadata]:
     :rtype: YouTubeMetadata
     """
     try:
-        metadata_rows: List = initial_data["contents"]["twoColumnWatchNextResults"][
-            "results"]["results"]["contents"][1]["videoSecondaryInfoRenderer"][
-            "metadataRowContainer"]["metadataRowContainerRenderer"]["rows"]
+        metadata_rows: List = initial_data["engagementPanels"][1]["engagementPanelSectionListRenderer"]["content"]["structuredDescriptionContentRenderer"]["items"][2]['horizontalCardListRenderer']['cards'][0]['videoAttributeViewModel']
     except (KeyError, IndexError):
-        # If there's an exception accessing this data, it probably doesn't exist.
-        return YouTubeMetadata([])
+        try:
+            metadata_rows: List = initial_data["engagementPanels"][2]["engagementPanelSectionListRenderer"]["content"]["structuredDescriptionContentRenderer"]["items"][2]['horizontalCardListRenderer']['cards'][0]['videoAttributeViewModel']
+        except (KeyError, IndexError):
+            # If there's an exception accessing this data, it probably doesn't exist.
+            return {'song': None, 'artist': None}
 
-    # Rows appear to only have "metadataRowRenderer" or "metadataRowHeaderRenderer"
-    #  and we only care about the former, so we filter the others
-    metadata_rows = filter(
-        lambda x: "metadataRowRenderer" in x.keys(),
-        metadata_rows
-    )
-
-    # We then access the metadataRowRenderer key in each element
-    #  and build a metadata object from this new list
-    metadata_rows = [x["metadataRowRenderer"] for x in metadata_rows]
-
-    return YouTubeMetadata(metadata_rows)
+    return {'song': metadata_rows['title'], 'artist': metadata_rows['subtitle']}
--- pytube/metadata.py
+++ pytube/metadata.py
@@ -15,11 +15,23 @@ class YouTubeMetadata:
             else:
                 continue
 
-            contents = el['contents'][0]
-            if 'simpleText' in contents:
-                self._metadata[-1][metadata_title] = contents['simpleText']
-            elif 'runs' in contents:
-                self._metadata[-1][metadata_title] = contents['runs'][0]['text']
+            if 'expandedMetadata' in el:
+                content = el['expandedMetadata']
+                if 'runs' in content:
+                    objects = []
+                    for o in content['runs']:
+                        if 'text' in o:
+                            if o['text'] != ', ':
+                                objects.append(o['text'])
+                    self._metadata[-1][metadata_title] = objects
+                elif 'simpleText' in content:
+                    self._metadata[-1][metadata_title] = el['expandedMetadata']['simpleText']
+            elif 'defaultMetadata' in el:
+                content = el['defaultMetadata']
+                if 'runs' in content:
+                    self._metadata[-1][metadata_title] = content['runs'][0]['text']
+                elif 'simpleText' in content:
+                    self._metadata[-1][metadata_title] = content['simpleText']
 
             # Upon reaching a dividing line, create a new grouping
             if el.get('hasDividerLine', False):
@@ -28,21 +40,3 @@ class YouTubeMetadata:
         # If we happen to create an empty dict at the end, drop it
         if self._metadata[-1] == {}:
             self._metadata = self._metadata[:-1]
-
-    def __getitem__(self, key):
-        return self._metadata[key]
-
-    def __iter__(self):
-        for el in self._metadata:
-            yield el
-
-    def __str__(self):
-        return json.dumps(self._metadata)
-
-    @property
-    def raw_metadata(self) -> Optional[Dict]:
-        return self._raw_metadata
-
-    @property
-    def metadata(self):
-        return self._metadata
